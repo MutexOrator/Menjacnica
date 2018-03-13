cee0aedd2d6fa02564083480924f945dcd5c3cac
MutexOrator <milosevic10116@fon.bg.ac.rs>
2018-03-13 19:04:54 +0100

    Dodati hashCode(), equals() i toString()

--- Menjacnica/src/menjacnica/Kurs.java
+++ Menjacnica/src/menjacnica/Kurs.java
@@ -31,5 +31,33 @@ public class Kurs {
 	public void setDatum(GregorianCalendar datum) {
 		this.datum = datum;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Kurs))
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Kurs [prodajni=" + prodajni + ", srednji=" + srednji + ", kupovni=" + kupovni + ", datum=" + datum
+				+ "]";
+	}
 	
 }
--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -24,5 +24,38 @@ public class Valuta {
 	public void setKurs(LinkedList<Kurs> kurs) {
 		this.kurs = kurs;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((punNaziv == null) ? 0 : punNaziv.hashCode());
+		result = prime * result + ((skrNaziv == null) ? 0 : skrNaziv.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Valuta))
+			return false;
+		Valuta other = (Valuta) obj;
+		if (punNaziv == null) {
+			if (other.punNaziv != null)
+				return false;
+		} else if (!punNaziv.equals(other.punNaziv))
+			return false;
+		if (skrNaziv == null) {
+			if (other.skrNaziv != null)
+				return false;
+		} else if (!skrNaziv.equals(other.skrNaziv))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Valuta [punNaziv=" + punNaziv + ", skrNaziv=" + skrNaziv + ", kurs=" + kurs + "]";
+	}
 	
 }
