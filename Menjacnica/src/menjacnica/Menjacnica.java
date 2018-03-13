6a119a9d4864e27a79179fa786460609b8d1fe6f
MutexOrator <milosevic10116@fon.bg.ac.rs>
2018-03-13 19:52:18 +0100

    Dodata klasa Menjacnica koja implementira InterfejsMenjacnica
    Metode jos nisu implementirane!

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import imenjacnica.InterfejsMenjacnica;
+
+public class Menjacnica implements InterfejsMenjacnica {
+
+	@Override
+	public void dodajKurs(GregorianCalendar datum, double prodajni, double srednji, double kupovni) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void brisanjeKursa(GregorianCalendar datum, double prodajni, double srednji, double kupovni) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kurs pronadjiKurs(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
